66415fc9ab60d276fc19ab1a2494281776e29790
SlimBenMiled <slim_sbm@hotmail.com>
2021-03-11 12:47:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state={person:{
+    fullName: "slim" ,
+    bio:"fsfqfqffsq" ,
+    imgSrc: <img  src="https://pbs.twimg.com/profile_banners/779973740616638464/1601910739/1500x500"/>,
+    
+    profession:'developper',
+    
+  },
+  show : true ,
+  count : 0,
+};
+componentDidMount(){
+  setInterval(() => {
+    this.setState({ count: this.state.count + 1 });
+  }, 1000);
 }
 
-export default App;
+  handleChange=()=>this.setState({show: !this.state.show,count:0})
+  render() {
+    return (
+ 
+
+      <div>
+      <button className="btn" onClick={()=>this.handleChange()}> {this.state.show ?"off":"on"} </button>  
+      {this.state.show ?(
+      <>
+           <div>
+<h1>Counter</h1>
+        <h2>{this.state.count}</h2>
+      </div>,
+      <h1> {this.state.person.fullName} </h1>
+      <h2>{this.state.person.bio}</h2>
+      <h2>{this.state.person.imgSrc}</h2>
+      <h2>{this.state.person.profession}</h2>
+ 
+      </>): null}
+      </div>
+
+    )
+  }
+}
